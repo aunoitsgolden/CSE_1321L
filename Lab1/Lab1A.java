3df6977f6d5631065a8928c3945b01c7a50c26af
go_rre <38592375+rowan-golden@users.noreply.github.com>
2023-08-18 14:13:41 -0400

    Lab1 CCSE1321L

---
+++ Lab1/Lab1A.java
@@ -0,0 +1,22 @@
+/*
+Class:      CSE1321L
+Section:    J03
+Term:       Fall 2023
+Instructor: Mourya
+Name:       Rowan Golden
+Lab#:       202
+ */
+
+// Program Lab1A
+// Demonstrate the difference between print and println.
+public class Lab1A {
+    public static void main (String[] args)
+    {
+        System.out.print ("Three... ");
+        System.out.print ("Two... ");
+        System.out.print ("One... ");
+        System.out.print ("Zero... ");
+        System.out.println ("Liftoff!"); // appears no the first line
+        System.out.println ("Houston, we have a problem");
+    }
+}
