bd9385c832553bc5b108676392981a896ba48ce1
go_rre <38592375+rowan-golden@users.noreply.github.com>
2023-08-25 14:12:10 -0400

    Add files via upload

---
+++ Module 2 - Lab/src/Lab2C.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Lab2C {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a width: ");
+        int width = input.nextInt();
+
+        System.out.print("Enter a height: ");
+        int height = input.nextInt();
+
+        int area = width*height;
+        int perimeter = 2*(width*height);
+
+        System.out.println("The area is "+area);
+        System.out.println("The perimeter is "+perimeter);
+    }
+}
