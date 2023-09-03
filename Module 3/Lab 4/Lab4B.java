024f3418370d3db262f549a4b7d42f92b5b4b19e
Rowan Golden <38592375+aunoitsgolden@users.noreply.github.com>
2023-09-03 14:30:52 -0400

    Add files via upload

---
+++ Module 3/Lab 4/Lab4B.java
@@ -0,0 +1,27 @@
+/*
+Class:      CSE1321L
+Section:    J03
+Term:       Fall 2023
+Instructor: Mourya
+Name:       Rowan Golden
+Lab#:       202
+*/
+import java.util.Scanner;
+
+class Lab4B {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter the day: ");
+    String today = (input.nextLine()).toLowerCase();
+    input.close();
+    
+    if (today.equals("monday") || today.equals("wednesday")) {
+      System.out.println("I have class today!");
+    } else if (today.equals("friday")) {
+      System.out.println("It's Friday! Friday! Gotta get down on Friday!");
+    } else {
+      System.out.println("I should use this time to do my homework.");
+    }
+  }
+}
---
+++ Module 3/Lab 4/Lab4C.java
@@ -0,0 +1,58 @@
+/*
+Class:      CSE1321L
+Section:    J03
+Term:       Fall 2023
+Instructor: Mourya
+Name:       Rowan Golden
+Lab#:       202
+*/
+import java.util.Scanner;
+
+class Lab4C {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    // Input
+    System.out.print("Welcome!"
+                    +"\nPlease input a number: ");
+    float number = input.nextInt();
+    
+    System.out.print("\nWhat would you like to do to this number:"
+                    +"\n0) Get the additive inverse of the number"
+                    +"\n1) Get the reciprocal of the number"
+                    +"\n2) Square the number"
+                    +"\n3) Cube the number"
+                    +"\n4) Exit the program"
+                    +"\n\n");
+        
+    int choice = input.nextInt();
+    input.close();
+
+    // Do Something + Output
+    float answer;
+    switch (choice) {
+      case 0:
+        answer = number*-1;
+        System.out.println("\nThe additive inverse of "+number+" is "+answer);
+        break;
+      case 1:
+        answer = 1/number;
+        System.out.println("\nThe reciprocal of "+number+" is "+answer);
+        break;
+      case 2:
+        answer = number*number;
+        System.out.println("\nThe square of "+number+" is "+answer);
+        break;
+      case 3:
+        answer = number*number*number;
+        System.out.println("\nThe cube of "+number+" is "+answer);
+        break;
+      case 4:
+        System.out.println("\nThank you, goodbye!");
+        break;
+      default: 
+        System.out.println("\nInvalid input, please try again!");
+        break;
+    }
+  }
+}
